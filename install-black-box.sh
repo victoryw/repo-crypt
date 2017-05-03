46dc9a35ff783bb7a2e97fffe415a7bde955e628
tianyawy <tianya_wy@hotmail.com>
2017-05-03 12:41:59 +0800

    [victoryw] #n/a init black box

--- install-black-box.sh
+++ install-black-box.sh
@@ -1,2 +1,6 @@
 brew install blackbox
+gpg2 --batch --generate-key ./gpg-key-store/create-key-config
 blackbox_initialize
+git commit -m'INITIALIZE BLACKBOX' keyrings ./.gitignore
+blackbox_addadmin tianya_wy@hotmail.com
+git commit -m'NEW ADMIN: tianya_wy@hotmail.com' keyrings/live/pubring.kbx keyrings/live/trustdb.gpg keyrings/live/blackbox-admins.txt
